67e7fa2aec38775f05932a1e0c51b644cfecbc08
Javi <jgjnez@gmail.com>
2026-04-18 09:39:52 -0500

    chore: Styles y semantic html basic for desing

--- projects/task_manager/src/main.js
+++ projects/task_manager/src/main.js
@@ -2,5 +2,21 @@ import './style.css';
 
 
 document.getElementById('app').innerHTML = `
-    <h1>Hola como estas, esta es la configuracion basica del proyecto.</h1>
+    <h1>Proyectos</h1>
+
+    <main class="project">
+        <section class="project-card">
+            <header class="card-header">
+                <div>
+                    <h3>Titulo del Proyecto</h3>
+                    <span>icono fecha</span>
+                </div>
+                <span>icono</span>
+            </header>
+            
+            <footer class="card-footer">
+                check 0 de total completadas
+            </footer>
+        </section>
+    </main>
 `;
