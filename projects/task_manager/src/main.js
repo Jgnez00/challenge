4e09a3b384d252f39dcfee03bd89f89d07432175
Javi <jgjnez@gmail.com>
2026-04-20 13:18:28 -0500

    feat: Render project list in DOM

--- projects/task_manager/src/main.js
+++ projects/task_manager/src/main.js
@@ -1,22 +1,47 @@
 import './style.css';
+import { projects } from './mocks.js';
 
 
 document.getElementById('app').innerHTML = `
-    <h1>Proyectos</h1>
-
-    <main class="project">
-        <section class="project-card">
-            <header class="card-header">
-                <div>
-                    <h3>Titulo del Proyecto</h3>
-                    <span>icono fecha</span>
-                </div>
-                <span>icono</span>
-            </header>
-            
-            <footer class="card-footer">
-                check 0 de total completadas
-            </footer>
-        </section>
+    <main class="container">
+        <h1>Proyectos</h1>
+        <ul id="projects" class="project-list"></ul>
     </main>
 `;
+
+const list_projectsE = document.getElementById('projects');
+
+
+(function lists () {
+    projects.forEach(project => {
+        const li = document.createElement('li');
+        const div = document.createElement('div');
+        const card_header = document.createElement('div');
+        const card_footer = document.createElement('div');
+        const span = document.createElement('span');
+        const spanIcon = document.createElement('span');
+        const h3 = document.createElement('h3');
+
+        h3.textContent = project.name;
+        span.textContent = 'icono y fecha';
+        spanIcon.textContent = 'icono';
+        card_footer.textContent = 'check 0 de total completadas';
+        card_footer.classList.add('card-footer');
+
+        div.appendChild(h3);
+        div.appendChild(span);
+
+        card_header.appendChild(div);
+        card_header.appendChild(spanIcon);
+        card_header.classList.add('card-header');
+
+
+        li.appendChild(card_header);
+        li.appendChild(card_footer);
+        li.classList.add('card');
+
+
+        list_projectsE.appendChild(li);
+        list_projectsE.classList.add('project-card');
+    });
+})()
---
+++ projects/task_manager/src/mocks.js
@@ -0,0 +1,32 @@
+export const projects = [
+    {
+        id: 1,
+        name: 'Name for project',
+        dueDate: "",
+        tasks: [
+            { id: 1, name: 'Task 1', priority: 'hight', completed: false },
+            { id: 2, name: 'Task 2', priority: 'low', completed: false },
+            { id: 3, name: 'Task 3', priority: 'medium', completed: false },
+        ]
+    },
+    {
+        id: 2,
+        name: 'Name for project',
+        dueDate: "",
+        tasks: [
+            { id: 4, name: 'Task 4', priority: 'hight', completed: false },
+            { id: 5, name: 'Task 5', priority: 'low', completed: false },
+            { id: 6, name: 'Task 6', priority: 'medium', completed: false },
+        ]
+    },
+    {
+        id: 3,
+        name: 'Name for project',
+        dueDate: "",
+        tasks: [
+            { id: 7, name: 'Task 7', priority: 'hight', completed: false },
+            { id: 8, name: 'Task 8', priority: 'low', completed: false },
+            { id: 9, name: 'Task 9', priority: 'medium', completed: false },
+        ]
+    }
+]
